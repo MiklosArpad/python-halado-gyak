eb13a2906a5ab162ec578a22297c17d8bf0c47ed
MiklosAprad <miklosarpad2007@gmail.com>
2021-02-20 15:32:56 +0100

    Dekorátorok 3. rész Az időzítővel ellátva 3 másodpercenként indítja a validálást

--- fuggveny_dekoratorok3.py
+++ fuggveny_dekoratorok3.py
@@ -2,7 +2,26 @@
 
 # function decorators - függvény dekorátorok 3. rész
 
-def validate_form(func):
-    def wrapper(password, email):
-        if len(password) < 6:
-            return ""
+import time
+
+def main_decorator(state):
+    def validate_form(func):
+        def wrapper(password, email):
+            print(state)
+            time.sleep(3)                                       # itt aktiváljuk az időzítőt
+            if len(password) < 6:
+                return "Weak password, at least 6 characters"
+            if '@' not in email:
+                return "Not valid email"
+            return func(password, email)
+        return wrapper
+    return validate_form
+
+
+@main_decorator("validating...")
+def form(password, email):
+    return "Your password is strong and you have a valid email"
+
+print(form("zazi", "1234567"))
+print(form("zazi@mail.hu", "1234567"))
+print(form("zazi@mail.hu", "123"))
