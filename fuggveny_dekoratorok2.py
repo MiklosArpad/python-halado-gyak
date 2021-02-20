d832c16e47b5357f5d01a8f16c6eaa321d56b2ce
MiklosAprad <miklosarpad2007@gmail.com>
2021-02-20 14:46:04 +0100

    Dekorátorok 2. rész

---
+++ fuggveny_dekoratorok2.py
@@ -0,0 +1,28 @@
+
+# function decorators - függvény dekorátorok 2. rész
+
+
+def validate_email(func):
+    def wrapper(email, password):
+        if '@' not in email:
+            return "Not valid email"
+        return func(email, password)
+    return wrapper
+
+def validate_password(func):
+    def wrapper(email, password):
+        if len(password) < 6:
+            return "Weak password, at least 6 characters"
+        return func(email, password)
+    return wrapper
+
+# itt lehet több dekorátort is magadni mint kettő
+
+@validate_email
+@validate_password
+def form(email, password):
+    return  "Your password is strong and you have a valid email"
+
+print(form("zazi", "1234567"))
+print(form("zazi@mail.hu", "1234567"))
+print(form("zazi@mail.hu", "123"))
